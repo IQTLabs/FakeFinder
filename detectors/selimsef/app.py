d3062888d8d547d75fd530175495273b15dd6df9
jaberkow <jackberkowitz88@gmail.com>
2021-02-17 18:07:49 +0000

    fixed filename-objectname error

--- detectors/selimsef/app.py
+++ detectors/selimsef/app.py
@@ -4,6 +4,7 @@ import pickle
 from flask import Flask, request, jsonify
 from ensemble import Ensemble
 import boto3
+import sys
 
 app = Flask(__name__)
 
@@ -15,17 +16,18 @@ s3 = boto3.resource('s3')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    video_list = str(request.get_json(force=True)['video_list'])
+    video_list = request.get_json(force=True)['video_list']
     predictions = []
-    for video in video_list:
+    for filename in video_list:
         score = 0.5
+        video = filename.rsplit('/',1)[-1]
         try:
             s3.Bucket(BUCKET_NAME).download_file(video, video)
             score = model.inference(video)
             os.remove(video)
         except:
             pass
-        predictions.append({'filename': video, 'prediction': score})
+        predictions.append({'filename': video, 'selimsef': score})
 
     result = pd.DataFrame(predictions)
     return result.to_json()
