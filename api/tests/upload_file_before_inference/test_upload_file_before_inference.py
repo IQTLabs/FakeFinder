7c2f9adc0da17ee20d4800db5e79cdbf3685bf83
cglewis <clewis@iqt.org>
2021-11-15 14:14:24 -0800

    simplify folder structure

--- api/tests/upload_file_before_inference/test_upload_file_before_inference.py
+++ api/tests/upload_file_before_inference/test_upload_file_before_inference.py
@@ -9,7 +9,7 @@ def test_upload_file_batch_mode_no_split_ntech():
     # Additional headers.
     headers = {'accept': 'application/json'}
 
-    files = {"file": ("file_test_4000.mp4", open("./api/tmp/file_test_4000.mp4", "rb"))}
+    files = {"file": ("file_test_4000.mp4", open("./api/tests/upload_file_before_inference/file_test_4000.mp4", "rb"))}
 
     resp = requests.post('http://localhost:5000/uploadS3/?bucket=ff-inbound-videos', headers=headers, files=files)
 
