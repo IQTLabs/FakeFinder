f544500e648b93025965ee23f293767a81a73d93
cglewis <clewis@iqt.org>
2021-11-15 15:22:11 -0800

    skip tests that require a gpu

--- api/tests/test_batch_mode_no_split_requests.py
+++ api/tests/test_batch_mode_no_split_requests.py
@@ -1,30 +1,31 @@
 import requests
 import json
 
-def test_batch_mode_ntech():
+def test_batch_mode_boken():
     url = 'http://localhost:5000/fakefinder/'
-    
+
     # Additional headers.
-    headers = {'Content-Type': 'application/json' } 
+    headers = {'Content-Type': 'application/json' }
 
     # Body
     payload = {"batchMode": True, 
                "alwaysOn": False,
                "s3Location": "s3://ff-inbound-videos/4000.mp4",
-               "modelName": "ntech",
+               "modelName": "boken",
                "splitRequests": False,
                "numSplitRequests": 0,
               }
-    
-    # convert dict to json string by json.dumps() for body data. 
-    resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))       
-    
+
+    # convert dict to json string by json.dumps() for body data.
+    resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))
+
     # Validate response headers and body contents, e.g. status code.
     assert resp.status_code == 200
-    
+
     # print response full body as text
     print(resp.json())
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_batch_mode_selimsef():
     url = 'http://localhost:5000/fakefinder/'
 
@@ -49,6 +50,7 @@ def test_batch_mode_selimsef():
     # print response full body as text
     print(resp.json())
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_batch_mode_medics():
     url = 'http://localhost:5000/fakefinder/'
 
@@ -73,6 +75,7 @@ def test_batch_mode_medics():
     # print response full body as text
     print(resp.json())
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_batch_mode_wm():
     url = 'http://localhost:5000/fakefinder/'
 
@@ -97,6 +100,7 @@ def test_batch_mode_wm():
     # print response full body as text
     print(resp.json())
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_batch_mode_eighteen():
     url = 'http://localhost:5000/fakefinder/'
 
--- api/tests/test_batch_mode_split_requests.py
+++ api/tests/test_batch_mode_split_requests.py
@@ -5,6 +5,7 @@ import pytest
 url = 'http://0.0.0.0:5000/fakefinder/'
 headers = {'Content-Type': 'application/json' }
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_batch_mode_ntech():
     # Body
     payload = {"batchMode": True,
@@ -25,12 +26,12 @@ def test_batch_mode_ntech():
     print(resp.json())
 
 @pytest.mark.parametrize('num_splits', [2, 4, 6, 10])
-def test_batch_mode_selimsef(num_splits):
+def test_batch_mode_boken(num_splits):
     # Body
     payload = {"batchMode": True,
                "alwaysOn": False,
                "s3Location": ["s3://ff-inbound-videos/4000.mp4", "s3://ff-inbound-videos/4001.mp4", "s3://ff-inbound-videos/4002.mp4", "s3://ff-inbound-videos/4003.mp4", "s3://ff-inbound-videos/4004.mp4", "s3://ff-inbound-videos/4005.mp4", "s3://ff-inbound-videos/4006.mp4", "s3://ff-inbound-videos/4007.mp4", "s3://ff-inbound-videos/4008.mp4", "s3://ff-inbound-videos/4009.mp4"],
-               "modelName": "selimsef",
+               "modelName": "boken",
                "splitRequests": True,
                "numSplitRequests": num_splits,
               }
@@ -44,6 +45,7 @@ def test_batch_mode_selimsef(num_splits):
     # print response full body as text
     print(resp.json())
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_batch_mode_medics():
     # Body
     payload = {"batchMode": True,
@@ -63,6 +65,7 @@ def test_batch_mode_medics():
     # print response full body as text
     print(resp.json())
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_batch_mode_wm():
     # Body
     payload = {"batchMode": True,
@@ -82,6 +85,7 @@ def test_batch_mode_wm():
     # print response full body as text
     print(resp.json())
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_batch_mode_eighteen():
     # Body
     payload = {"batchMode": True,
